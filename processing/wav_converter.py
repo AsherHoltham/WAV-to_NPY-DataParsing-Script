649fe1fe09c00c85f4f06f27639116450f13c27f
AsherHoltham <asher.holtham.github@gmail.com>
2024-11-18 15:02:49 -0800

    changed the wav to npy conversion formula

--- processing/wav_converter.py
+++ processing/wav_converter.py
@@ -1,9 +1,16 @@
 import librosa
 import numpy as np
 
-def converter(ifile):
-    audio, _ = librosa.load(ifile)
-    output = np.abs(librosa.stft(audio))
+def converter(ifile, sr=22050, n_fft=2048, hop_length=512, use_log_scale=True):
+    audio, sr = librosa.load(ifile, sr=sr)
+    stft = librosa.stft(audio, n_fft=n_fft, hop_length=hop_length)
+    magnitude = np.abs(stft)
+    
+    if use_log_scale:
+        output = librosa.amplitude_to_db(magnitude, ref=np.max)
+    else:
+        output = magnitude
+
     return output
 
 
